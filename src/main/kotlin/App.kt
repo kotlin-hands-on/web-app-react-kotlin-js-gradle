e71f1642969041805f0575ae452a38e70d061838
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-02-05 13:53:04 +0100

    Move HTML to App component Introduce VideoList component with selection mechanism (state and properties)

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,44 @@
+import kotlinx.css.*
+import react.*
+import react.dom.*
+import styled.css
+import styled.styledDiv
+
+@JsExport
+class App : RComponent<RProps, RState>() {
+    override fun RBuilder.render() {
+        h1 {
+            +"KotlinConf Explorer"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            videoList {
+                videos = unwatchedVideos
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            videoList {
+                videos = watchedVideos
+            }
+        }
+        styledDiv {
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/VideoList.kt
@@ -0,0 +1,41 @@
+import kotlinx.html.js.onClickFunction
+import kotlinx.browser.window
+import react.*
+import react.dom.*
+
+
+external interface VideoListProps: RProps {
+    var videos: List<Video>
+}
+
+external interface VideoListState: RState {
+    var selectedVideo: Video?
+}
+
+@JsExport
+class VideoList: RComponent<VideoListProps, VideoListState>() {
+    override fun RBuilder.render() {
+        for (video in props.videos) {
+            p {
+                key = video.id.toString()
+                attrs {
+                    onClickFunction = {
+                        setState {
+                            selectedVideo = video
+                        }
+                    }
+                }
+                if(video == state.selectedVideo) {
+                    +"▶ "
+                }
+                +"${video.speaker}: ${video.title}"
+            }
+        }
+    }
+}
+
+fun RBuilder.videoList(handler: VideoListProps.() -> Unit): ReactElement {
+    return child(VideoList::class) {
+        this.attrs(handler)
+    }
+}
