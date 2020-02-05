140d3ec137d9e8a46cb8181b5cfd6e421c94f967
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-02-05 13:25:27 +0100

    Add typesafe HTML representation of website Use styled components Add Video data class and example lists for unwatched and watched videos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,70 @@
+import react.dom.*
 import kotlinx.browser.document
+import kotlinx.css.*
+import styled.*
+
+external interface Video {
+    val id: Int
+    val title: String
+    val speaker: String
+    val videoUrl: String
+}
+
+data class KotlinVideo(
+    override val id: Int,
+    override val title: String,
+    override val speaker: String,
+    override val videoUrl: String
+) : Video
+
+val unwatchedVideos = listOf(
+    KotlinVideo(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+    KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+    KotlinVideo(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+)
+
+val watchedVideos = listOf(
+    KotlinVideo(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+)
 
 fun main() {
-    document.bgColor = "blue"
+    render(document.getElementById("root")) {
+        h1 {
+            +"KotlinConf Explorer"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            for(video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            for(video in watchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+        }
+        styledDiv {
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+    }
 }
