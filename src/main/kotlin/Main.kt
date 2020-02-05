daf1370b93fe6ca716830c44a84e771cc4c423f2
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-02-05 13:53:04 +0100

    Move HTML to App component Introduce VideoList component with selection mechanism (state and properties)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,42 +17,6 @@ val watchedVideos = listOf(
 
 fun main() {
     render(document.getElementById("root")) {
-        h1 {
-            +"KotlinConf Explorer"
-        }
-        div {
-            h3 {
-                +"Videos to watch"
-            }
-            for(video in unwatchedVideos) {
-                p {
-                    +"${video.speaker}: ${video.title}"
-                }
-            }
-
-            h3 {
-                +"Videos watched"
-            }
-            for(video in watchedVideos) {
-                p {
-                    +"${video.speaker}: ${video.title}"
-                }
-            }
-        }
-        styledDiv {
-            css {
-                position = Position.absolute
-                top = 10.px
-                right = 10.px
-            }
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                attrs {
-                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-                }
-            }
-        }
+        child(App::class) {}
     }
 }
