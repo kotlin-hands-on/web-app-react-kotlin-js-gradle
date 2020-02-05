608b912b7e762dcfe4872b39230c00f892fdc318
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-02-05 13:58:58 +0100

    Lift currentVideo state to app, remove video list state Pass video selection handlers to video list

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -4,27 +4,23 @@ import react.*
 import react.dom.*
 
 
-interface VideoListProps: RProps {
+external interface VideoListProps: RProps {
     var videos: List<Video>
-}
-
-interface VideoListState: RState {
     var selectedVideo: Video?
+    var onSelectVideo: (Video) -> Unit
 }
 
-class VideoList: RComponent<VideoListProps, VideoListState>() {
+class VideoList: RComponent<VideoListProps, RState>() {
     override fun RBuilder.render() {
         for (video in props.videos) {
             p {
                 key = video.id.toString()
                 attrs {
                     onClickFunction = {
-                        setState {
-                            selectedVideo = video
-                        }
+                        props.onSelectVideo(video)
                     }
                 }
-                if(video == state.selectedVideo) {
+                if(video == props.selectedVideo) {
                     +"▶ "
                 }
                 +"${video.speaker}: ${video.title}"
