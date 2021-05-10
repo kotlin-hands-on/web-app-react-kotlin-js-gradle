20fa9f27ccb4886da0635de88b273c62589d3fd8
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2021-05-10 16:43:06 +0200

    Update Kotlin, Gradle, React/JS.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.4.31"
+    id("org.jetbrains.kotlin.js") version "1.5.0"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
     maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
     mavenCentral()
     jcenter()
@@ -26,14 +27,14 @@ kotlin {
 dependencies {
 
     //React, React DOM + Wrappers (chapter 3)
-    implementation("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.21")
-    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.21")
-    implementation(npm("react", "17.0.1"))
-    implementation(npm("react-dom", "17.0.1"))
+    implementation("org.jetbrains:kotlin-react:17.0.2-pre.154-kotlin-1.5.0")
+    implementation("org.jetbrains:kotlin-react-dom:17.0.2-pre.154-kotlin-1.5.0")
+    implementation(npm("react", "17.0.2"))
+    implementation(npm("react-dom", "17.0.2"))
 
     //Kotlin Styled (chapter 3)
-    implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.21")
-    implementation(npm("styled-components", "~5.2.1"))
+    implementation("org.jetbrains:kotlin-styled:5.2.3-pre.154-kotlin-1.5.0")
+    implementation(npm("styled-components", "~5.2.3"))
 
     //Video Player (chapter 7)
     implementation(npm("react-youtube-lite", "1.0.1"))
@@ -42,7 +43,7 @@ dependencies {
     implementation(npm("react-share", "~4.2.1"))
 
     //Coroutines (chapter 8)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-RC")
 }
 
 // Heroku Deployment (chapter 9)
