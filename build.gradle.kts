c5bcf87f2c163eba7458fe568c94b0833df63aff
Saeed Momenzadeh <saied.momen@balad.ir>
2021-08-19 20:14:47 +0430

    update dependencies and fix webpack issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.5.0"
+    id("org.jetbrains.kotlin.js") version "1.5.21"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("https://dl.bintray.com/kotlin/kotlin-eap")
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
-    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
     mavenCentral()
-    jcenter()
+    google()
 }
 
 kotlin {
-    js {
+    js(IR) {
         browser {
             commonWebpackConfig {
                 cssSupport.enabled = true
@@ -27,14 +24,14 @@ kotlin {
 dependencies {
 
     //React, React DOM + Wrappers (chapter 3)
-    implementation("org.jetbrains:kotlin-react:17.0.2-pre.154-kotlin-1.5.0")
-    implementation("org.jetbrains:kotlin-react-dom:17.0.2-pre.154-kotlin-1.5.0")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.231-kotlin-1.5.21")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.231-kotlin-1.5.21")
     implementation(npm("react", "17.0.2"))
     implementation(npm("react-dom", "17.0.2"))
 
     //Kotlin Styled (chapter 3)
-    implementation("org.jetbrains:kotlin-styled:5.2.3-pre.154-kotlin-1.5.0")
-    implementation(npm("styled-components", "~5.2.3"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.0-pre.231-kotlin-1.5.21")
+    implementation(npm("styled-components", "~5.3.0"))
 
     //Video Player (chapter 7)
     implementation(npm("react-youtube-lite", "1.0.1"))
@@ -49,4 +46,9 @@ dependencies {
 // Heroku Deployment (chapter 9)
 tasks.register("stage") {
     dependsOn("build")
+}
+
+//fix for webpack issue from https://youtrack.jetbrains.com/issue/KT-48273#focus=Comments-27-5122487.0-0
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().versions.webpackDevServer.version = "4.0.0-rc.0"
 }
