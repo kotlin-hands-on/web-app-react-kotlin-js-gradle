da2af7541dd60607125e4ccfa88515f1a234ea3a
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-03-22 16:24:10 +0300

    Using Kotlin language constructs in markup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,72 @@
 import kotlinx.browser.document
-import react.Fragment
-import react.create
-import react.dom.html.ReactHTML.div
-import react.dom.html.ReactHTML.h1
-import react.dom.html.ReactHTML.h3
-import react.dom.html.ReactHTML.img
 import react.dom.html.ReactHTML.p
-import react.dom.render
+
 
 fun main() {
+
+    data class Video(
+        val id: Int,
+        val title: String,
+        val speaker: String,
+        val videoUrl: String
+    )
+
+    val unwatchedVideos = listOf(
+        Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+        Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+        Video(5, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg"),
+        Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+    )
+
+    val watchedVideos = listOf(
+        Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+    )
+
+    for (video in unwatchedVideos) {
+        p {
+            +"${video.speaker}: ${video.title}"
+        }
+    }
+    for (video in watchedVideos) {
+        p {
+            +"${video.speaker}: ${video.title}"
+        }
+    }
+
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
 
-    render(Fragment.create {
-        h1 {
-            +"Hello, React+Kotlin/JS!"
-        }
-        div {
-            h3 {
-                +"Videos to watch"
-            }
-            p {
-                +"John Doe: Building and breaking things"
-            }
-            p {
-                +"Jane Smith: The development process"
-            }
-            p {
-                +"Matt Miller: The Web 7.0"
-            }
-
-            h3 {
-                +"Videos watched"
-            }
-            p {
-                +"Tom Jerry: Mouseless development"
-            }
-        }
-        div {
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-            }
-        }
-    }, container)
+//    render(Fragment.create {
+//        h1 {
+//            +"Hello, React+Kotlin/JS!"
+//        }
+//        div {
+//            h3 {
+//                +"Videos to watch"
+//            }
+//            p {
+//                +"John Doe: Building and breaking things"
+//            }
+//            p {
+//                +"Jane Smith: The development process"
+//            }
+//            p {
+//                +"Matt Miller: The Web 7.0"
+//            }
+//
+//            h3 {
+//                +"Videos watched"
+//            }
+//            p {
+//                +"Tom Jerry: Mouseless development"
+//            }
+//        }
+//        div {
+//            h3 {
+//                +"John Doe: Building and breaking things"
+//            }
+//            img {
+//                src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+//            }
+//        }
+//    }, container)
 }
