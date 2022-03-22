b78cf610790e59f9aa601e953f8bc2ae577067aa
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-03-22 16:12:58 +0300

    Building typesafe HTML using domain-specific language (DSL)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,48 @@
 import kotlinx.browser.document
+import react.Fragment
+import react.create
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.h3
+import react.dom.html.ReactHTML.img
+import react.dom.html.ReactHTML.p
+import react.dom.render
 
 fun main() {
-    document.bgColor = "red"
+    val container = document.getElementById("root") ?: error("Couldn't find root container!")
+
+    render(Fragment.create {
+        h1 {
+            +"Hello, React+Kotlin/JS!"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            p {
+                +"John Doe: Building and breaking things"
+            }
+            p {
+                +"Jane Smith: The development process"
+            }
+            p {
+                +"Matt Miller: The Web 7.0"
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            p {
+                +"Tom Jerry: Mouseless development"
+            }
+        }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+            }
+        }
+    }, container)
 }
