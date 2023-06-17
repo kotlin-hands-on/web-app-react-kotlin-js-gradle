626a5d78aa493fcc54286207a127deab45f36ad9
Danil Pavlov <danil.pavlov@jetbrains.com>
2023-06-17 13:20:02 +0200

    update: kotlin multiplatform plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("js") version "1.8.20"
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("multiplatform") version "1.8.22"
+    kotlin("plugin.serialization") version "1.8.22"
 }
 
 group = "org.example"
@@ -21,26 +21,29 @@ kotlin {
         }
         binaries.executable()
     }
-}
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                //React, React DOM + Wrappers (chapter 3)
+                implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.430"))
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
 
-dependencies {
-    //React, React DOM + Wrappers (chapter 3)
-    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.430"))
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
+                //Kotlin React Emotion (CSS) (chapter 3)
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion")
 
-    //Kotlin React Emotion (CSS) (chapter 3)
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion")
+                //Video Player (chapter 7)
+                implementation(npm("react-player", "2.12.0"))
 
-    //Video Player (chapter 7)
-    implementation(npm("react-player", "2.12.0"))
+                //Share Buttons (chapter 7)
+                implementation(npm("react-share", "4.4.1"))
 
-    //Share Buttons (chapter 7)
-    implementation(npm("react-share", "4.4.1"))
-
-    //Coroutines & serialization (chapter 8)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+                //Coroutines & serialization (chapter 8)
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+            }
+        }
+    }
 }
 
 // Heroku Deployment (chapter 9)
